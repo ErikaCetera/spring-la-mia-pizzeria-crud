b994e0ba3ef16ed7d7d60a5c72cecd13204f38d9
Erika Cetera <ceteraerika@gmail.com>
2025-04-10 18:48:48 +0200

    feat: add crud show

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -25,5 +26,12 @@ public class PizzaController {
         List<Pizza> pizze = repository.findAll();
         model.addAttribute("pizze", pizze);
         return "pizze/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(Model model, @PathVariable("id") Integer id){
+        Pizza pizza = repository.findById(id).get();
+        model.addAttribute("pizza", pizza);
+       return "pizze/show";
     }
 }
